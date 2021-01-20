aff419a3252559cc94016e958a3b435b82aec836
SaloniYerawar15 <77059802+SaloniYerawar15@users.noreply.github.com>
2021-01-20 22:45:59 +0530

    Create Matrix.c
    Creating and printing the elements of matrix and displaying the sum of principal diagonal elements

---
+++ Matrix.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void main()
+{
+  int arr1[3][3],i,j,rows,Sum;
+  
+       
+  
+ 
+    /* Stored values into the array*/
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+        printf("element - [%d],[%d] : ",i,j);
+        scanf("%d",&arr1[i][j]);
+      }
+  }  
+ 
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr1[i][j]);
+  }
+  
+  
+  for(rows= 0; rows < i; rows++)  
+   {    
+      Sum = Sum + arr1[rows][rows];  
+   }  
+   printf("\n The Sum of Diagonal Elements of a Matrix = %d", Sum );  
+   return 0;
+ 
+}
